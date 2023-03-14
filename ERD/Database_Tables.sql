332caaac3f9761ad87c2a67aff05f88e9a3ed52e
YashuGv <venkatayasaswini66@gmail.com>
2023-03-14 14:45:06 +0530

    Api for PatientHealthRecordService

--- ERD/Database_Tables.sql
+++ ERD/Database_Tables.sql
@@ -1,19 +1,27 @@
 CREATE TABLE [PatientBasicRecord](
     [Id] UNIQUEIDENTIFIER ,
     [DateTime] smalldatetime,
-    [Patient_Id] UNIQUEIDENTIFIER PRIMARY KEY,
-    [Nurse_Id] UNIQUEIDENTIFIER,
+    [Patient_Id] NVARCHAR(100) PRIMARY KEY,
+    [Nurse_Id] NVARCHAR(max),
     [Bp] NVARCHAR(max),
     [Heart_Rate] int,
     [SpO2] NVARCHAR(max) 
 );
 
 
+CREATE table [PatientAllergy](
+    [Id] UNIQUEIDENTIFIER PRIMARY KEY,
+    [Health_Id] NVARCHAR(100),
+    [Allergy] NVARCHAR(max),
+    CONSTRAINT [Fk_PatientAllergy]  FOREIGN Key(Health_Id) REFERENCES [PatientBasicRecord](Patient_Id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
+
 CREATE TABLE [PatientHealthRecord](
     [Id] UNIQUEIDENTIFIER PRIMARY KEY,
     [DateTime] smalldatetime,
-    [Patient_Id] UNIQUEIDENTIFIER,
-    [Doctor_Id] UNIQUEIDENTIFIER,
+    [Patient_Id] NVARCHAR(max),
+    [Doctor_Id] NVARCHAR(max),
     [Conclusion] NVARCHAR(max) 
 );
 
@@ -32,15 +40,8 @@ Create TABLE [PatientTest](
     CONSTRAINT [Fk_patientTest] FOREIGN KEY(Health_Id) REFERENCES [PatientHealthRecord](Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE table [PatientAllergy](
-    [Id] UNIQUEIDENTIFIER PRIMARY KEY,
-    [Health_Id] UNIQUEIDENTIFIER,
-    [Allergy] NVARCHAR(max),
-    CONSTRAINT [Fk_PatientAllergy]  FOREIGN Key(Health_Id) REFERENCES [PatientBasicRecord](Patient_Id) ON DELETE CASCADE ON UPDATE CASCADE
-)
-
 SELECT * FROM PatientBasicRecord
+SELECT * FROM PatientAllergy
 SELECT * FROM PatientHealthRecord
 SELECT * FROM PatientMedication
 SELECT * FROM PatientTest
-SELECT * FROM PatientAllergy
