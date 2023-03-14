101df3ffc5e369dd164ae97052d843c4868570d7
GeFF <71188123+GeFF-Git@users.noreply.github.com>
2023-03-15 02:29:03 +0530

    AppointmentService Commit-1

---
+++ AppointmentService/AppointmentService_API/DataLayer/Entities/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer.Entities;
+
+public partial class Appointment
+{
+    public Guid AppointmentId { get; set; }
+
+    public string? PatientId { get; set; }
+
+    public string? DoctorId { get; set; }
+
+    public string? NurseId { get; set; }
+
+    public short? Status { get; set; }
+}
