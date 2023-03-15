c9209483db80a694792a906e7a21fab7ade92caa
mahathisomisetty <mahathisomisetty777@gmail.com>
2023-03-15 11:31:02 +0530

    angular updated code

---
+++ PatientHealthRecordServices/PatientHealthRecordAngular/HospitalM/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HealthinfoComponent } from './healthinfo/healthinfo.component';
+import { LoginComponent } from './login/login.component';
+import { PatientMedicationComponent } from './patient-medication/patient-medication.component';
+import { PatientbasicrecordComponent } from './patientbasicrecord/patientbasicrecord.component';
+import { RegisterComponent } from './register/register.component';
+
+
+const routes: Routes = [
+  
+{path:'',component:RegisterComponent},
+
+{
+  path:'login',component:LoginComponent
+},
+{
+  path:'register',component:RegisterComponent
+},
+
+{
+  path:'patientbasicrecord',component:PatientbasicrecordComponent
+},
+{
+  path:'patient-medication',component:PatientMedicationComponent
+}, 
+{
+  path:'dashboard',component:DashboardComponent
+},
+{
+  path:'healthinfo',component:HealthinfoComponent
+}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ PatientHealthRecordServices/PatientHealthRecordAngular/HospitalM/src/app/healthinfo/healthinfo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-healthinfo',
+  templateUrl: './healthinfo.component.html',
+  styleUrls: ['./healthinfo.component.css']
+})
+export class HealthinfoComponent {
+
+}
