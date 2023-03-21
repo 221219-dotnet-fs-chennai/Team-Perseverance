35b543672c5a01ba1cbde2437548d1830446ebde
bhanuprakashkola26 <bhanuprakashkola26@gmail.com>
2023-03-21 18:12:54 +0530

    Changes made

--- complete-ui/src/app/components/nurse/nurse-nav/updateinfo/updateinfo.component.ts
+++ complete-ui/src/app/components/nurse/nurse-nav/updateinfo/updateinfo.component.ts
@@ -8,14 +8,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./updateinfo.component.css']
 })
 export class UpdateinfoComponent {
-  constructor(private router : Router){}
+  constructor(private router: Router){}
+  
   toppings = new FormControl('');
   toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
   updateform = new FormGroup({
     bp: new FormControl('', [Validators.required,Validators.pattern('^[1-9][0-9]{1,2}\\/[1-9][0-9]{1,2}$')]),
     rate: new FormControl('', [Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|120)$')]),
-    spo2: new FormControl('', [Validators.required,Validators.pattern('^(9[0-9]|100)%$')])
+    spo2: new FormControl('', [Validators.required,Validators.pattern('^(9[0-9]|100)%$')]),
+    height: new FormControl('',[Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|250)$')]),
+    weight: new FormControl('', [Validators.required, Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|350)$')]),
+    temp: new FormControl('', [Validators.required, Validators.pattern('^([3-9][0-9]|[1-9][0-9]{2})\.?[0-9]?$')])
   });
 
   update() {
@@ -25,7 +29,6 @@ export class UpdateinfoComponent {
     } else {
       // show error message
       console.log(this.updateform);
-      
       console.log('Invalid form');
     }
   }
@@ -46,6 +49,18 @@ export class UpdateinfoComponent {
     return this.updateform.get('spo2');
   }
 
+  get height() {
+    return this.updateform.get('height');
+  }
+
+  get weight() {
+    return this.updateform.get('weight');
+  }
+
+  get temp() {
+    return this.updateform.get('temp');
+  }
+
   getErrorMessage(formControlName: string) {
     if (this.updateform.get(formControlName)?.hasError('required')) {
       return 'This field is required';
@@ -56,7 +71,13 @@ export class UpdateinfoComponent {
         return 'Please enter a valid heart rate reading between 60 and 120 bpm';
       } else if (formControlName === 'spo2') {
         return 'Please enter a valid SpO2 reading between 90% and 100%';
-      } else {
+      } else if (formControlName === 'height') {
+        return 'Please enter a valid height reading like 126.8';
+      } else if (formControlName === 'weight'){
+        return 'Please enter a valid height reading like 90';
+      }else if (formControlName === 'temp'){
+        return 'Please enter a valid height reading like 37.1';
+      }else{
         return 'Invalid input';
       }
     }
