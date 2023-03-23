73314a96ed25fa71d958f9b5c7ded8070d889a06
unknown <prasanna4223@gmail.com>
2023-03-23 18:44:03 +0530

    modified api

--- PatientIfoService/DataLogic/Entities/Patientinfo.cs
+++ PatientIfoService/DataLogic/Entities/Patientinfo.cs
@@ -19,9 +19,9 @@ public partial class Patientinfo
 
     public long? Phone { get; set; }
 
-    public string? Adress { get; set; }
+    public string? AdressLine { get; set; }
+
+    public string? City { get; set; }
 
     public string? State { get; set; }
-
-    public string? Country { get; set; }
 }
--- PatientIfoService/DataLogic/IRepo.cs
+++ PatientIfoService/DataLogic/IRepo.cs
@@ -1,9 +1,9 @@
-﻿using DataLogic.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLogic.Entities;
 using Models;
 namespace DataLogic
 {
@@ -12,6 +12,6 @@ namespace DataLogic
         public IEnumerable<PatientInfo> GetallPatientinfos();
         public IEnumerable<PatientInfo> GetPatientinfosbyemail(string Email);
         public void AddnewPatientInfo(Patientinfo patientinfo);
-        public PatientInfo updatePatientinfos( Guid Pat_id, Patientinfo patientinfo);
+        public PatientInfo updatePatientinfos(Guid Pat_id, Patientinfo patientinfo);
     }
 }
