53c006cb7c3ddaad4241f6c747080f5e404029cc
GeFF <71188123+GeFF-Git@users.noreply.github.com>
2023-03-23 16:52:57 +0530

    API Consume Appointment

--- complete-ui/src/app/components/nurse/nurse-nav/updateinfo/updateinfo.component.ts
+++ complete-ui/src/app/components/nurse/nurse-nav/updateinfo/updateinfo.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { AppointmentServiceService } from 'src/app/services/appointment-service/appointment-service.service';
+import { Guid } from 'guid-typescript';
+import { localStorageToken } from '../../../patient/show-doctors/localstorage.token';
+import { AppointmentDoctor } from 'src/app/models/appointmentServiceModel';
 @Component({
   selector: 'app-updateinfo',
   templateUrl: './updateinfo.component.html',
   styleUrls: ['./updateinfo.component.css']
 })
 export class UpdateinfoComponent {
-  constructor(private router: Router){}
-  
+  constructor(private router: Router, private appointmentService : AppointmentServiceService,
+    @Inject(localStorageToken) private localStorage : any){}
+
+  disableSubmit : boolean = true;
+
   toppings = new FormControl('');
   toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
@@ -17,15 +23,35 @@ export class UpdateinfoComponent {
     bp: new FormControl('', [Validators.required,Validators.pattern('^[1-9][0-9]{1,2}\\/[1-9][0-9]{1,2}$')]),
     rate: new FormControl('', [Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|120)$')]),
     spo2: new FormControl('', [Validators.required,Validators.pattern('^(9[0-9]|100)%$')]),
-    height: new FormControl('',[Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|250)$')]),
+    height: new FormControl('',[Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|150)$')]),
     weight: new FormControl('', [Validators.required, Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|350)$')]),
-    temp: new FormControl('', [Validators.required, Validators.pattern('^([3-9][0-9]|[1-9][0-9]{2})\.?[0-9]?$')])
+    temp: new FormControl('', [Validators.required, Validators.pattern('^([3-9][0-9]|[1-9][0-9]{2})\.?[0-9]?$')]),
+    bloodgroup: new FormControl('',[Validators.required]),
+    // allergies: new FormControl('',[Validators.required]),
+
   });
+
+  updateNurseIdByNurse(){
+    this.appointmentService.updateNurseIdByNurse(this.localStorage.getItem('app_id for nurse'),'3245b75e-2398-4a8d-8617-44fa42f534c2')
+    .subscribe({
+      next : (appointment) => {
+        console.log(appointment);
+      },
+      error: (response) => {
+        console.log(response);
+      }
+
+    });
+  }
+
 
   update() {
     console.log(this.updateform);
+
     if (this.updateform.valid) {
+
       console.warn(this.updateform.value);
+      this.disableSubmit = false;
     } else {
       // show error message
       console.log(this.updateform);
@@ -60,6 +86,14 @@ export class UpdateinfoComponent {
   get temp() {
     return this.updateform.get('temp');
   }
+  get bloodgroup(){
+    return this.updateform.get('bloodgroup')
+  }
+
+  // get allergies(){
+  //   return this.updateform.get('toppings')
+  // }
+
 
   getErrorMessage(formControlName: string) {
     if (this.updateform.get(formControlName)?.hasError('required')) {
@@ -82,5 +116,7 @@ export class UpdateinfoComponent {
       }
     }
     return '';
+
+
   }
 }
